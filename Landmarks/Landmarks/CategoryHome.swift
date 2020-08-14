36e7748e17d68c7e0f181d4b590c197b55e876d1
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-08-14 19:06:36 -0300

    Section 1 Add a Home View

---
+++ Landmarks/Landmarks/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Felipe de Souza Longo on 14/08/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
