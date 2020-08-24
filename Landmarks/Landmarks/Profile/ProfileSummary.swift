4a28bb6ba7cd98e02c2084e46e693a91fdb416f8
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-08-24 19:05:21 -0300

    Section 1 Display a User Profile

---
+++ Landmarks/Landmarks/Profile/ProfileSummary.swift
@@ -0,0 +1,65 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Felipe de Souza Longo on 24/08/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    static let goalFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On" : "Off")")
+            
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+            
+            VStack(alignment: .leading) {
+                Text("Completed Badges")
+                    .font(.headline)
+                ScrollView {
+                    HStack {
+                        HikeBadge(name: "First Hike")
+                        
+                        HikeBadge(name: "Earth Day")
+                            .hueRotation(/*@START_MENU_TOKEN@*/Angle(degrees: 90)/*@END_MENU_TOKEN@*/)
+                        
+                        HikeBadge(name: "Tenth Hike")
+                            .grayscale(0.5)
+                            .hueRotation(Angle(degrees: 45))
+                    }
+                }
+            }
+            .frame(height: 140)
+            
+            VStack(alignment: .leading) {
+                Text("Recent Hikes")
+                    .font(.headline)
+                
+                HikeView(hike: hikeData[0])
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: .default)
+    }
+}
